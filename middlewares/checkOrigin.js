3fa478fe63d1a813a2567b3e2dd85ec0d6bd1c7c
ankit-slt <ankit.kumar@soliterasoftware.com>
2025-04-14 07:12:45 +0530

    Port updated version 1.3

--- middlewares/checkOrigin.js
+++ middlewares/checkOrigin.js
@@ -37,30 +37,38 @@ const STATUS = require("../constants/STATUS");
 
 const validateRequest = (req, res, next) => {
     const requestOrigin = req.headers.origin;
+    const requestHost = req.headers.host;
+    const requestReferer = req.headers.referer || "";
 
-    // Split allowed origins string into array
-    const allowedOrigins = process.env.PROD_ALLOW_ORIGIN.split(',');
+    const allowedOrigins = process.env.PROD_ALLOW_ORIGIN
+        ? process.env.PROD_ALLOW_ORIGIN.split(",")
+        : [];
 
-    console.log("Allowed Origins:", allowedOrigins);
-    console.log("Request Origin:", requestOrigin);
-    console.log("Request Headers:", req.headers);
+    const isAllowed =
+        allowedOrigins.includes(requestOrigin) ||
+        (requestOrigin === undefined &&
+            (requestHost?.includes("internal.hironai.com") ||
+             requestReferer?.includes("internal.hironai.com")));
 
+    // console.log("Allowed Origins:", allowedOrigins);
+    // console.log("Request Origin:", requestOrigin);
+    // console.log("Request Host:", requestHost);
+    // console.log("Request Referer:", requestReferer);
 
-    if (allowedOrigins.includes(requestOrigin)) {
-        // ✅ Set headers
-        res.setHeader("Access-Control-Allow-Origin", requestOrigin);
+    if (isAllowed) {
+        // Set CORS headers conditionally only if origin exists
+        if (requestOrigin) {
+            res.setHeader("Access-Control-Allow-Origin", requestOrigin);
+        }
         res.setHeader("Access-Control-Allow-Credentials", "true");
         res.setHeader("Access-Control-Allow-Methods", "GET,HEAD,PUT,PATCH,POST,DELETE");
         res.setHeader("Access-Control-Allow-Headers", "Content-Type,Authorization");
-
-        res.setHeader("HironAi", "Origin"); // Optional custom header
-        next();
+        return next();
     } else {
         return res
             .status(STATUS.FORBIDDEN)
             .json({ success: false, error: req.t("Forbidden: Access Not Allowed") });
     }
 };
-
 
 module.exports = validateRequest;
